06301afdb49b30a684e786d9160ddcd9b1c108ba
troystribling <troy.stribling@gmail.com>
2010-11-14 17:58:23 -0500

    created project

---
+++ Classes/Views/MapScene.h
@@ -0,0 +1,21 @@
+//
+//  MapScene.h
+//  seeker1
+//
+//  Created by Troy Stribling on 11/14/10.
+//  Copyright imaginary products 2010. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorld Layer
+@interface MapScene : CCLayer
+{
+}
+
+// returns a Scene that contains the HelloWorld as the only child
++(id) scene;
+
+@end
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  seeker1
+//
+//  Created by Troy Stribling on 11/14/10.
+//  Copyright imaginary products 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"Seeker1AppDelegate");
+	[pool release];
+	return retVal;
+}
