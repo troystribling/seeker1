b6f4ecbbc8702648957254f4a17fd01d59a7814a
troystribling <troy.stribling@gmail.com>
2010-12-24 21:55:06 -0500

    more work on mission display

--- Classes/Views/QuadsScene.h
+++ Classes/Views/QuadsScene.h
@@ -11,14 +11,31 @@
 #import "cocos2d.h"
 
 //-----------------------------------------------------------------------------------------------------------------------------------
+@class StatusDisplay;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 @interface QuadsScene : CCLayer {
-
+    StatusDisplay* statusDisplay;
+    CCSprite* tharsisSprite;
+    CGPoint tharsisPosition;
+    CCSprite* memnoniaSprite;
+    CGPoint memnoniaPosition;
+    CCSprite* elysiumSprite;
+    CGPoint elysiumPosition;
+    CCSprite* displayedSprite; 
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------
+@property (nonatomic, retain) StatusDisplay* statusDisplay;
+@property (nonatomic, retain) CCSprite* tharsisSprite;
+@property (nonatomic, assign) CGPoint tharsisPosition;
+@property (nonatomic, retain) CCSprite* memnoniaSprite;
+@property (nonatomic, assign) CGPoint memnoniaPosition;
+@property (nonatomic, retain) CCSprite* elysiumSprite;
+@property (nonatomic, assign) CGPoint elysiumPosition;
+@property (nonatomic, assign) CCSprite* displayedSprite;
 
 //-----------------------------------------------------------------------------------------------------------------------------------
++ (id)scene;
 
 @end
--- Classes/Views/QuadsScene.m
+++ Classes/Views/QuadsScene.m
@@ -8,6 +8,10 @@
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 #import "QuadsScene.h"
+#import "StatusDisplay.h"
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+#define kMISSION_SET_IMAGE_YDELTA       50.0f
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 @interface QuadsScene (PrivateAPI)
@@ -17,6 +21,14 @@
 @implementation QuadsScene
 
 //-----------------------------------------------------------------------------------------------------------------------------------
+@synthesize statusDisplay;
+@synthesize tharsisSprite;
+@synthesize tharsisPosition;
+@synthesize memnoniaSprite;
+@synthesize memnoniaPosition;
+@synthesize elysiumSprite;
+@synthesize elysiumPosition;
+@synthesize displayedSprite;
 
 //===================================================================================================================================
 #pragma mark QuadsScene PrivateAPI
@@ -35,8 +47,32 @@
 //-----------------------------------------------------------------------------------------------------------------------------------
 - (id)init {
 	if( (self=[super init] )) {
+        self.elysiumSprite = [[[CCSprite alloc] initWithFile:@"elysium.png"] autorelease];
+        self.memnoniaSprite = [[[CCSprite alloc] initWithFile:@"memnonia.png"] autorelease];
+        self.tharsisSprite = [[[CCSprite alloc] initWithFile:@"tharsis.png"] autorelease];
+        self.displayedSprite = self.tharsisSprite;
+        self.statusDisplay = [StatusDisplay createWithFile:@"empty-display.png"];
+        [self.statusDisplay insert:self];
+        [self.statusDisplay addTerminalText:@"$ miss"];
+        [self schedule:@selector(nextFrame:)];
     }
 	return self;
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+- (void) nextFrame:(ccTime)dt {
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+-(void) ccTouchesBegan:(NSSet*)touches withEvent:(UIEvent *)event {
+}    
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+-(void) ccTouchesEnded:(NSSet*)touches withEvent:(UIEvent *)event {
+}    
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+- (void)ccTouchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {    
 }
 
 @end
