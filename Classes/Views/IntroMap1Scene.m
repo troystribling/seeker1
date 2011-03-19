ca9de8b301aee8a6a108a23b6115b96f0d166263
troystribling <troy.stribling@gmail.com>
2011-03-18 22:19:48 -0400

    more work on first intro

--- Classes/Views/IntroMap1Scene.m
+++ Classes/Views/IntroMap1Scene.m
@@ -11,7 +11,7 @@
 #import "IntroMap2Scene.h"
 
 //-----------------------------------------------------------------------------------------------------------------------------------
-#define kMAX_TAPS           10
+#define kMAX_TAPS           11
 #define kINTRO_1_TICK_1     30
 #define kTAP_COUNTER_DELTA  180
 
@@ -42,8 +42,10 @@
 //-----------------------------------------------------------------------------------------------------------------------------------
 -(void)showTapToContinue {
     self.acceptTouches = YES;
-    CCSprite* tapSprite = [CCSprite spriteWithFile:@"tap-to-continue.png"];
-    [self addChild:tapSprite];
+    CGSize screenSize = [[CCDirector sharedDirector] winSize];
+    self.tapCounterMessageSprite = [CCSprite spriteWithFile:@"tap-to-continue.png"];
+    self.tapCounterMessageSprite.position = CGPointMake(screenSize.width/2, 15.0);
+    [self addChild:self.tapCounterMessageSprite];
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------
@@ -52,9 +54,6 @@
     self.messageDisplayedCount = self.counter;
     if (self.tapCounter > 1) {
         [self.displayedMessageSprite removeFromParentAndCleanup:YES];
-    }
-    if (self.tapCounterMessageSprite) {
-        [self.tapCounterMessageSprite removeFromParentAndCleanup:YES];
     }
     if (self.itemSprite) {
         [self.itemSprite removeFromParentAndCleanup:YES];
@@ -94,6 +93,9 @@
             self.displayedMessageSprite = [CCSprite spriteWithFile:@"map1-text-11.png"];
             break;
     }
+    CGSize screenSize = [[CCDirector sharedDirector] winSize];
+    self.displayedMessageSprite.position = CGPointMake(screenSize.width/2, 290.0);
+    self.displayedMessageSprite.anchorPoint = CGPointMake(0.5, 0.5);
     [self addChild:self.displayedMessageSprite];
 }
 
@@ -117,7 +119,7 @@
         self.tapCounterMessageSprite = nil;
         self.isTouchEnabled = YES;
         self.itemSprite = nil;
-        CCSprite* backgroundGrid = [CCSprite spriteWithFile:@"empty-map.png"];
+        CCSprite* backgroundGrid = [CCSprite spriteWithFile:@"intro-map.png"];
         backgroundGrid.anchorPoint = CGPointMake(0.0, 0.0);
         backgroundGrid.position = CGPointMake(0.0, 0.0);
         [self addChild:backgroundGrid z:-10];
@@ -135,7 +137,7 @@
     if (self.counter == kINTRO_1_TICK_1) {
         self.tapCounter++;
         [self showMessage];
-    } else if ((self.counter - self.messageDisplayedCount) > kTAP_COUNTER_DELTA) {
+    } else if ((self.counter - self.messageDisplayedCount) > kTAP_COUNTER_DELTA && self.tapCounterMessageSprite == nil) {
         [self showTapToContinue];
     }    
 }
@@ -145,6 +147,10 @@
     if (self.acceptTouches) {
         self.tapCounter++;
         if (self.tapCounter <= kMAX_TAPS) {
+            if (self.tapCounterMessageSprite) {
+                [self.tapCounterMessageSprite removeFromParentAndCleanup:YES];
+            }
+            self.tapCounterMessageSprite = nil;
             [self showMessage];
         } else {
             [[CCDirector sharedDirector] replaceScene: [IntroMap2Scene scene]];
