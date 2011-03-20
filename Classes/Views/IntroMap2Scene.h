63e250209073cf26ad3339d8459ad61dc629a42d
troystribling <troy.stribling@gmail.com>
2011-03-19 22:26:25 -0400

    started second tutorial map

--- Classes/Views/IntroMap2Scene.h
+++ Classes/Views/IntroMap2Scene.h
@@ -11,17 +11,34 @@
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 @class StatusDisplay;
+@class SeekerSprite;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 @interface IntroMap2Scene : CCLayer {
     StatusDisplay* statusDisplay;
+    CCSprite* displayedMessageSprite;
+    CCSprite* tapCounterMessageSprite;
+    CCSprite* sensorSiteSprite;
+    CCSprite* sensorSprite;
+    SeekerSprite* seeker;
     NSInteger counter;
+    NSInteger messageDisplayedCount;;
+    NSInteger tapCounter;
+    NSInteger energy;
     BOOL acceptTouches;
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 @property (nonatomic, retain) StatusDisplay* statusDisplay;
+@property (nonatomic, retain) CCSprite* displayedMessageSprite;
+@property (nonatomic, retain) CCSprite* tapCounterMessageSprite;
+@property (nonatomic, retain) CCSprite* sensorSiteSprite;
+@property (nonatomic, retain) CCSprite* sensorSprite;
+@property (nonatomic, retain) SeekerSprite* seeker;
 @property (nonatomic, assign) NSInteger counter;
+@property (nonatomic, assign) NSInteger messageDisplayedCount;
+@property (nonatomic, assign) NSInteger tapCounter;
+@property (nonatomic, assign) NSInteger energy;
 @property (nonatomic, assign) BOOL acceptTouches;
 
 //-----------------------------------------------------------------------------------------------------------------------------------
--- Classes/Views/IntroMap2Scene.m
+++ Classes/Views/IntroMap2Scene.m
@@ -8,13 +8,23 @@
 //-----------------------------------------------------------------------------------------------------------------------------------
 #import "IntroMap2Scene.h"
 #import "StatusDisplay.h"
+#import "SeekerSprite.h"
 
 //-----------------------------------------------------------------------------------------------------------------------------------
-#define kINTRO_1_TICK_1    40
-#define kINTRO_1_TICK_2    40
+#define kMAX_TAPS           1
+#define kINTRO_2_TICK_1     30
+#define kITEM_COUNTER_DELTA 60
+#define kTAP_COUNTER_DELTA  180
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 @interface IntroMap2Scene (PrivateAPI)
+
+- (void)initObjects;
+- (void)updateEnergy;
+- (void)updateSensorCount;
+- (void)initStatusDisplay;
+-(void)showTapToContinue;
+-(void)showMessage;
 
 @end
 
@@ -23,11 +33,85 @@
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 @synthesize statusDisplay;
+@synthesize displayedMessageSprite;
+@synthesize tapCounterMessageSprite;
+@synthesize sensorSiteSprite;
+@synthesize sensorSprite;
+@synthesize seeker;
+@synthesize messageDisplayedCount;
+@synthesize tapCounter;
 @synthesize counter;
+@synthesize energy;
 @synthesize acceptTouches;
 
 //===================================================================================================================================
 #pragma mark IntroMap2Scene PrivateAPI
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+- (void)initObjects {
+    self.seeker = [SeekerSprite create];
+    CGSize screenSize = [[CCDirector sharedDirector] winSize];
+    [self.seeker setToStartPoint:CGPointMake(screenSize.width/2.0 - 10.0, 240.0) withBearing:@"south"];
+    CCSprite* homeBase = [CCSprite spriteWithFile:@"map2-home-base.png"];
+    homeBase.position = CGPointMake(screenSize.width/2.0 - 40.0, 210.0);
+    self.sensorSiteSprite = [CCSprite spriteWithFile:@"map2-sensor-site.png"];
+    self.sensorSiteSprite.position = CGPointMake(screenSize.width/2.0 - 10.0, 60.0);
+    [self addChild:homeBase z:-1];
+    [self addChild:self.seeker z:0];
+    [self addChild:self.sensorSiteSprite z:0];
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+- (void)updateEnergy {
+    self.energy -= 2;
+    [self.statusDisplay setDigits:self.energy forDisplay:EnergyDisplayType]; 
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+- (void)updateSensorCount {
+    [self.statusDisplay setDigits:0 forDisplay:SensorDisplayType]; 
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+- (void)initStatusDisplay {
+    [self.statusDisplay setDigits:1 forDisplay:LevelDisplayType]; 
+    [self.statusDisplay setDigits:0 forDisplay:SampleDisplayType]; 
+    [self.statusDisplay setDigits:15 forDisplay:SpeedDisplayType]; 
+    [self.statusDisplay setDigits:1 forDisplay:SensorDisplayType]; 
+    [self.statusDisplay setDigits:self.energy forDisplay:EnergyDisplayType]; 
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+-(void)showTapToContinue {
+    self.acceptTouches = YES;
+    CGSize screenSize = [[CCDirector sharedDirector] winSize];
+    self.tapCounterMessageSprite = [CCSprite spriteWithFile:@"tap-to-continue.png"];
+    self.tapCounterMessageSprite.position = CGPointMake(screenSize.width/2.0, 15.0);
+    [self addChild:self.tapCounterMessageSprite];
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+-(void)showMessage {
+    self.acceptTouches = NO;
+    self.messageDisplayedCount = self.counter;
+    self.tapCounter++;
+    if (self.tapCounterMessageSprite) {
+        [self.tapCounterMessageSprite removeFromParentAndCleanup:YES];
+        self.tapCounterMessageSprite = nil;
+    }
+    if (self.tapCounter > 1) {
+        [self.displayedMessageSprite removeFromParentAndCleanup:YES];
+    }
+    switch (self.tapCounter) {
+        case 1:
+            self.displayedMessageSprite = [CCSprite spriteWithFile:@"map2-text-1.png"];
+            break;
+    }
+    CGSize screenSize = [[CCDirector sharedDirector] winSize];
+    self.displayedMessageSprite.position = CGPointMake(screenSize.width/2, 285.0);
+    self.displayedMessageSprite.anchorPoint = CGPointMake(0.5, 0.5);
+    [self addChild:self.displayedMessageSprite];
+}
 
 //===================================================================================================================================
 #pragma mark IntroMap2Scene
@@ -44,13 +128,17 @@
 - (id)init {
 	if((self=[super init])) {
         self.counter = 0;
+        self.energy = 6;
+        self.tapCounter = 0;
+        self.messageDisplayedCount = kINTRO_2_TICK_1;
         CCSprite* backgroundGrid = [CCSprite spriteWithFile:@"empty-map.png"];
         backgroundGrid.anchorPoint = CGPointMake(0.0, 0.0);
         backgroundGrid.position = CGPointMake(0.0, 0.0);
         [self addChild:backgroundGrid z:-10];
         self.statusDisplay = [StatusDisplay createWithFile:@"empty-display.png"];
         [self.statusDisplay insert:self];
-        [self.statusDisplay test];
+        [self initObjects];
+        [self initStatusDisplay];
         [self schedule:@selector(nextFrame:)];
     }
 	return self;
@@ -59,14 +147,20 @@
 //-----------------------------------------------------------------------------------------------------------------------------------
 - (void) nextFrame:(ccTime)dt {
     self.counter++;
-    if (self.counter == kINTRO_1_TICK_1) {
-    } else if (self.counter == kINTRO_1_TICK_2) {
+    if (self.counter == kINTRO_2_TICK_1) {
+        [self showMessage];
+    } else if ((self.counter - self.messageDisplayedCount) > kTAP_COUNTER_DELTA && self.tapCounterMessageSprite == nil) {
+        [self showTapToContinue];
     }    
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 -(void) ccTouchesBegan:(NSSet*)touches withEvent:(UIEvent *)event {
     if (self.acceptTouches) {
+        if (self.tapCounter < kMAX_TAPS) {
+            [self showMessage];
+        } else {
+        }
     }
 }    
 
