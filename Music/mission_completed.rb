b54b6c2845646f53d07e458aed344375a071dec5
troystribling <troy.stribling@gmail.com>
2011-04-08 00:13:22 -0400

    more music work

--- Music/mission_completed.rb
+++ Music/mission_completed.rb
@@ -10,8 +10,10 @@ end
 # completed
 #.........................................................................................................
 completed_p1 = [np([:C,5], :mixolydian, :l=>8, :v=>0.99)[4,6,2], n(:R, :l=>8)]
-completed_p2 = [np([:D,5], :aeolian, :l=>8, :v=>0.99)[4,6,2], n(:R, :l=>8)]
-completed = [completed_p1, completed_p2]
+completed_p2 = [np([:D,5], :aeolian, :l=>16, :v=>0.99)[4,5,6], n(:R, :l=>16), np([:D,5], :aeolian, :l=>16, :v=>0.99)[6], n(:R, :l=>16)]
+completed_p3 = [np([:D,5], :aeolian, :l=>16, :v=>0.99)[5,4,2], n(:R, :l=>16)]
+completed_p4 = [np([:C,5], :mixolydian, :l=>8, :v=>0.99)[3,1,3], n(:R, :l=>8)]
+completed = [completed_p1, completed_p2, completed_p3, completed_p4]
 
 #.........................................................................................................
 str 'completed' do
