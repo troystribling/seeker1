d5e6ff77a57a032ab3e31a21dfdbaf33f30e4c58
troystribling <troy.stribling@gmail.com>
2011-04-08 22:36:16 -0400

    took a bug slash in music composition

--- Music/mission_completed.rb
+++ Music/mission_completed.rb
@@ -7,18 +7,18 @@ before_start do
 end
 
 #.........................................................................................................
-# completed
+# lead
 #.........................................................................................................
-completed_p1 = [np([:C,5], :mixolydian, :l=>8, :v=>0.99)[4,6,2], n(:R, :l=>8)]
-completed_p2 = [np([:D,5], :aeolian, :l=>16, :v=>0.99)[4,5,6], n(:R, :l=>16), np([:D,5], :aeolian, :l=>16, :v=>0.99)[6], n(:R, :l=>16)]
-completed_p3 = [np([:D,5], :aeolian, :l=>16, :v=>0.99)[5,4,2], n(:R, :l=>16)]
-completed_p4 = [np([:C,5], :mixolydian, :l=>8, :v=>0.99)[3,1,3], n(:R, :l=>8)]
-completed = [completed_p1, completed_p2, completed_p3, completed_p4]
+lead_p1 = []
+4.times do |i|
+  lead_p1 << np([:As,4], :mixolydian, :l=>16, :v=>0.99)[i+1,i+2,i+3,i+4]
+end
+lead = [lead_p1]
 
 #.........................................................................................................
-str 'completed' do
-  c = (count - 1) % completed.length
-  ch(0) << completed[c]
+str 'feature' do
+  c = (count - 1) % lead.length
+  ch(0) << lead[c]
 end
 
 play
