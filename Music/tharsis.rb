3d3d1386ddeddf59347d4fffb34613908c005bfa
troystribling <troy.stribling@gmail.com>
2011-04-10 15:39:03 -0400

    finished tharsis started elysium

--- Music/tharsis.rb
+++ Music/tharsis.rb
@@ -30,7 +30,7 @@ start = 1
 rhythm_p1 = [n(:R, :l=>1), n(:R, :l=>8)]
 rhythm_p2 = [n(:R, :l=>4), np([:D,5], :mixolydian, :l=>8, :v=>0.99)[4,2]]
 rhythm_p3 = [n(:R, :l=>8), np([:D,5], :aeolian, :l=>8, :v=>0.99)[3,5]]
-rhythm_p4 = [n(:R, :l=>8), np([:D,5], :aeolian, :l=>16, :v=>0.99)[4,2,3], n(:R, :l=>16), n(:R, :l=>8)]
+rhythm_p4 = [n(:R, :l=>8), np([:D,5], :aeolian, :l=>16, :v=>0.99)[4,2,3,1], n(:R, :l=>8)]
 rhythm_phrases =[rhythm_p1, rhythm_p2, rhythm_p3, rhythm_p2, rhythm_p3, rhythm_p2, rhythm_p3, rhythm_p2, rhythm_p3, rhythm_p4, rhythm_p1]
 
 #.........................................................................................................
@@ -43,11 +43,12 @@ end
 # atmos
 #.........................................................................................................
 start = 1
-atmos_p1 = [n(:R), n([:C,5]), n(:R), n([:C,5]), n(:R, :l=>8)]
-atmos_p2 = [n([:C,5]), n(:R)]
-atmos_p3 = [n([:C,5]), n(:R, :l=>8)]
-atmos_p4 = [n([:C,5]), n(:R)]
-atmos_phrases = [atmos_p1, atmos_p2, atmos_p3, atmos_p2, atmos_p3, atmos_p2, atmos_p3, atmos_p2, atmos_p3, atmos_p4, atmos_p1]
+atmos_p1 = [n(:R, :l=>1), n(:R, :l=>8)]
+atmos_p2 = [np([:C,5], :mixolydian, :l=>4)[1], n(:R)]
+atmos_p3 = [np([:C,5], :mixolydian, :l=>4)[2], n(:R, :l=>8)]
+atmos_p4 = [np([:C,5], :mixolydian, :l=>4)[1], n(:R)]
+atmos_p5 = [np([:C,5], :mixolydian, :l=>4)[4], n(:R), np([:C,5], :mixolydian, :l=>4)[2], n(:R), n(:R, :l=>8)]
+atmos_phrases = [atmos_p1, atmos_p2, atmos_p3, atmos_p2, atmos_p3, atmos_p2, atmos_p3, atmos_p2, atmos_p3, atmos_p4, atmos_p5]
 #.........................................................................................................
 str 'atmos' do
   c = (count - 1) % atmos_phrases.length
